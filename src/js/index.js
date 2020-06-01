e0a0df1646e8e74066eb0eb5e9957c3ef2cb786e
Adrian C <adek2222@hotmail.com>
2020-06-01 11:17:00 +0200

    new logo

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 const mobileMenu = document.querySelector('.hamburger');
+const menuList = document.querySelector('.menu__list');
 
 mobileMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('hamburger--active');
+  menuList.classList.toggle('menu__list--active');
 });
